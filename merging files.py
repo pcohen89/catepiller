366d71b629393ca69c861a5e8d6547b618f7309e
p_cohen <peter.cohen.uchicago@gmail.com>
2015-08-14 09:05:42 -0500

    rationalized file merging to make it more auditable

--- merging files.py
+++ merging files.py
@@ -38,41 +38,79 @@ def merge_subms(subm_dict, path, name, target):
         subm = subm.drop('target2', 1)
     subm.to_csv(path+name, index=False)
 
+def check_weight_and_merge(dict, name):
+    """
+    :param dict: file, weight pairs
+    :param name: name of resulting blended file
+    :return: blended file saved to server
+    """
+    total_weight = 0
+    for key, val in dict.iteritems():
+        total_weight += val
+    print "The total weight should be 1.0, it is: %s" % (total_weight)
+    merge_subms(dict, SUBM_PATH, name, 'cost')
+
+
 ####################### Run Code #########################
+old_files_to_merge = {
+    'threeway vars with subsamp.csv': .08,
+    'threeway vars with forest.csv': .1,
+    '1500 trees xgb.csv': .1,
+    '2500 trees xgb.csv': .1,
+    '2500 trees xgb w spec vars.csv': .1,
+    '2500 trees xgb w extra vars.csv': .07,
+    'stacking with xgboost second stage all vars.csv': .07,
+    'stacking with all vars in forest.csv': .08,
+    'xgboost from first data build.csv': .08,
+    'stacking with higher eta.csv': .07,
+    'stacking with ridge vars.csv': .07,
+    'cv stack with new vars frst second stage internal 243.csv': .08
+}
+
+files2500_to_merge = {
+    '2500 trees power bill vars.csv': .08,
+    'weight 2500 trees.csv': .31,
+    '2500 with new folds.csv': .38,
+    'downweighted 2500 trees.csv': .23,
+
+}
+
+hightrees_to_merge = {
+    '4000 trees power bill vars 2nd set depth 8.csv': .27,
+    'upweighted 4000 trees.csv': .07,
+    'upweighted 6500 trees no compset.csv': .13,
+    '6500 with new folds.csv': .33,
+    'downweighted 4000 trees.csv': .04,
+    'downweighted 6500 trees w compset.csv': .05,
+    '4000 trees power bill vars.csv': .05,
+    '4000 trees power no log.csv': .06,
+}
+
+stacking_to_merge = {
+  'upweight stack extra stage1 vars.csv': .4,
+  'threeway vars with bill vars.csv': .2,
+  'stack w new folds.csv': .4
+}
+
+check_weight_and_merge(old_files_to_merge, 'old files.csv')
+check_weight_and_merge(files2500_to_merge, '2500 files.csv')
+check_weight_and_merge(hightrees_to_merge, 'high tree files.csv')
+check_weight_and_merge(stacking_to_merge, 'stacking files.csv')
 
 # Merge submissions
 subs_to_merge = {
-    '4000 trees power bill vars 2nd set depth 8.csv': .06,
-    'upweighted 4000 trees.csv': .1,
-    'upweighted 6500 trees no compset.csv': .07,
-    'downweighted 4000 trees.csv': .06,
-    'downweighted 6500 trees w compset.csv': .01,
-    '4000 trees power bill vars.csv': .03,
-    '4000 trees power no log.csv': .02,
-    '2500 trees power bill vars.csv': .04,
-    'weight 2500 trees.csv': .11,
-    'downweighted 2500 trees.csv': .035,
-    'threeway vars with bill vars.csv': .33,
-    'threeway vars with subsamp.csv': .07,
-    'threeway vars with forest.csv': .005,
-    '1500 trees xgb.csv': .01,
-    '2500 trees xgb.csv': .01,
-    '2500 trees xgb w spec vars.csv': .005,
-    '2500 trees xgb w extra vars.csv': .005,
-    'stacking with xgboost second stage all vars.csv': .005,
-    'stacking with all vars in forest.csv': .005,
-    'xgboost from first data build.csv': .005,
-    'stacking with higher eta.csv': .005,
-    'stacking with ridge vars.csv': .005,
-    'cv stack with new vars frst second stage internal 243.csv': .005
+    'high tree files.csv': .4,
+    'stacking files.csv': .35,
+    '2500 files.csv': .15,
+    'old files.csv': .1
 }
 
 total_weight = 0
 for key, val in subs_to_merge.iteritems():
     total_weight += val
 print "The total weight should be 1.0, it is: %s" % (total_weight)
-merge_subms(subs_to_merge, SUBM_PATH, 'blend x.csv', 'cost')
+merge_subms(subs_to_merge, SUBM_PATH, 'blend z.csv', 'cost')
 
-subm_correl('blend x.csv',
-            'blend w.csv', 'id', 'cost')
+subm_correl('blend y.csv',
+            'blend z.csv', 'id', 'cost')
 
