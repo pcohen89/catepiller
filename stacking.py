8a1cdda713cc94013940bbe810388e29532dc759
p_cohen <peter.cohen.uchicago@gmail.com>
2015-08-29 13:32:47 -0500

    added a more classic blender

--- stacking.py
+++ stacking.py
@@ -3,7 +3,7 @@ from __builtin__ import list, range, len, str, set, any, int, min
 
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import Lasso, Ridge
+from sklearn.linear_model import Lasso, Ridge, RidgeCV
 import numpy as np
 import sys
 sys.path.append('/home/vagrant/xgboost/wrapper')
@@ -136,6 +136,13 @@ def gen_weights(df):
     df = df.drop('one', axis=1)
     return df
 
+def create_feat_list(df, non_features):
+    feats = list(df.columns.values)
+    for var in non_features:
+        feats.remove(var)
+    return feats
+
+
 ######################################################
 # Load data
 all_data = pd.read_csv(CLN_PATH + "full_data.csv")
@@ -145,6 +152,12 @@ test = all_data[all_data.is_test != 0]
 # Set hyper parameters of process
 types = ['boss', 'adaptor', 'elbow', 'float', 'hfl', 'nut', 'other', 'sleeve',
          'straight', 'threaded']
+
+################
+# Globals
+################
+non_feats = ['id', 'is_test', 'tube_assembly_id', 'cost']
+
 all_feats = all_data.columns.values
 avg_score = 0
 first_loop = 0
@@ -163,10 +176,7 @@ for cv_fold in range(start_num, start_num+num_loops):
     feat_trn, mod_trn = create_val_and_train(trn, cv_fold, 'tube_assembly_id', .15)
     feat_trn = gen_weights(feat_trn)
     # Create list of second stage modeling features
-    stage2_feats = list(all_data.columns.values)
-    non_feats = ['id', 'is_test', 'tube_assembly_id', 'cost']
-    for var in non_feats:
-        stage2_feats.remove(var)
+    stage2_feats = create_feat_list(all_data, non_feats)
     # Gradient boosting (choose TWO comp types a generate models on using those
     # and core features)
     for i in range(0, len(types)):
@@ -240,6 +250,98 @@ for cv_fold in range(start_num, start_num+num_loops):
     score = rmsle(val['cost'], val['preds'+str(cv_fold)])
     print "Score for fold %s is: %s" % (cv_fold, score)
     avg_score += score/num_loops
+print avg_score
+
+
+test['cost'] = test[[u'preds42', u'preds43', u'preds44', u'preds45', u'preds46']].mean(axis=1)
+
+
+# Export test preds
+test['id'] = test['id'].apply(lambda x: int(x))
+test[['id', 'cost']].to_csv(SUBM_PATH+'stack w col samp 5 folds new var.csv', index=False)
+
+
+######################################################
+# New Stacking Concept
+#####################################################
+all_data = pd.read_csv(CLN_PATH + "full_data.csv")
+non_test = all_data[all_data.is_test == 0]
+test = all_data[all_data.is_test != 0]
+
+# Set hyper parameters of process
+avg_score = 0
+first_loop = 0
+num_loops = 1
+start_num = 42
+# Create list of second stage modeling features
+stage1_feats = create_feat_list(all_data, non_feats)
+stage2_feats = create_feat_list(all_data, non_feats)
+for frststage_mod in ['rdg', 'xgb', 'frst']:
+    stage2_feats.append('preds_' + frststage_mod)
+# Run bagged models
+for cv_fold in range(start_num, start_num+num_loops):
+    param = {'max_depth': 6, 'eta': .135, 'silent': 1, 'subsample': .7,
+             'colsample_bytree': .5}
+    # Create trn val samples
+    trn, val = create_val_and_train(non_test, cv_fold, 'tube_assembly_id', .2)
+    # recode target variable to log(x+1) in trn and val
+    trn['target'] = np.power(trn.cost, .0625)
+    val['target'] = np.power(val.cost, .0625)
+    # Separate samples for first stage and second stage
+    feat_trn, mod_trn = create_val_and_train(trn, cv_fold, 'tube_assembly_id', .25)
+    feat_trn = gen_weights(feat_trn)
+    # Gradient boosting
+    # Create xgboost data sets
+    xgb_feat_trn = xgb.DMatrix(np.array(feat_trn[stg1_feats]),
+                               label=np.array(feat_trn.target),
+                               weight=np.array(feat_trn.ob_weight))
+    xgb_mod_trn = xgb.DMatrix(np.array(mod_trn[stg1_feats]),
+                              label=np.array(mod_trn.target))
+    xgb_val = xgb.DMatrix(np.array(val[stg1_feats]))
+    xgb_test = xgb.DMatrix(np.array(test[stg1_feats]))
+    # Fit xgboost
+    xboost = xgb.train(param.items(), xgb_feat_trn, 100)
+    # Create scaled predictions
+    val = write_xgb_preds(val, xgb_val, xboost, '_xgb')
+    mod_trn = write_xgb_preds(mod_trn, xgb_mod_trn, xboost, '_xgb')
+    test = write_xgb_preds(test, xgb_test, xboost, '_xgb')
+    # Create ridge feats
+    alphas = [.5, 1.5, 3, 4.5]
+    model = RidgeCV(alphas=alphas).fit(feat_trn[stg1_feats], feat_trn.target)
+    # Predict and rescale predictions
+    val = write_preds(val, model, '_rdg', stg1_feats)
+    mod_trn = write_preds(mod_trn, model, '_rdg', stg1_feats)
+    test = write_preds(test, model, '_rdg', stg1_feats)
+    # Create forest feats
+    frst = RandomForestRegressor(n_estimators=200, n_jobs=8)
+    frst.fit(feat_trn[stg1_feats], feat_trn.target)
+    # Predict and rescale predictions
+    val = write_preds(val, frst, '_frst', stg1_feats)
+    mod_trn = write_preds(mod_trn, frst, '_frst', stg1_feats)
+    test = write_preds(test, frst, '_frst', stg1_feats)
+    # Score models
+    score_xgb = rmsle(val.cost, val.preds_xgb)
+    score_rdg = rmsle(val.cost, val.preds_rdg)
+    score_frst = rmsle(val.cost, val.preds_frst)
+    # Report score of loop
+    label1 = "XGB score is %s;"
+    label2 = "Ridge score is %s;"
+    label3 = "Forest score is %s;"
+    label = label1 + label2 + label3
+    print label % (score_xgb, score_rdg, score_frst)
+    # Fit second stage model
+    model = RandomForestRegressor(n_estimators=200, n_jobs=8)
+    model.fit(mod_trn[stage2_feats], mod_trn.target.values)
+    val = write_preds(val, model, cv_fold, stage2_feats)
+    test = write_preds(test, model, cv_fold, stage2_feats)
+    # Score loop
+    score = rmsle(val['cost'], val['preds'+str(cv_fold)])
+    print "Score for fold %s is: %s" % (cv_fold, score)
+    avg_score += score/num_loops
+    outputs = pd.DataFrame({'feats': stage2_feats,
+                        'weight': model.feature_importances_})
+    outputs = outputs.sort(columns='weight', ascending=False)
+    print outputs
 print avg_score
 
 
